f6a72797f7c43f9fc1f9e2b932207967e94c54bc
Joakim Erik Åkerman <joakim.akerman@hotmail.com>
2024-11-12 12:20:26 +0100

    Added ScrollToTop.js and minor adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { DarkModeProvider } from './common/DarkModeContext';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Construction from './pages/Construction';
+import ScrollToTop from './common/ScrollToTop';
 
 function App() {
   return (
     <DarkModeProvider>
       <BrowserRouter basename="/Silicon-JS-frontend">
+        <ScrollToTop/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Contact" element={<Contact />} />
